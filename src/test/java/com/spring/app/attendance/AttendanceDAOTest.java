d6379058470dba7c88c34c12281453c93ae6a78b
rlajinwon <wlsdnjs652@naver.com>
2025-05-30 12:03:48 +0900

    05-30 근태관리

--- src/test/java/com/spring/app/attendance/AttendanceDAOTest.java
+++ src/test/java/com/spring/app/attendance/AttendanceDAOTest.java
@@ -3,12 +3,24 @@ package com.spring.app.attendance;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+@SpringBootTest
 class AttendanceDAOTest {
+	
+	
+    @Autowired
+    private AttendanceService service;
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+	
+		
+		
+		
 	}
 
 }
