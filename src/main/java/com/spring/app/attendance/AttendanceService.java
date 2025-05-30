4896ac198d11d2fd2f669c2dd4e50e88c53f064a
rlajinwon <wlsdnjs652@naver.com>
2025-05-30 11:59:41 +0900

    05-30 근태관리 서비스까지

---
+++ src/main/java/com/spring/app/attendance/AttendanceService.java
@@ -0,0 +1,5 @@
+package com.spring.app.attendance;
+
+public class AttendanceService {
+
+}
---
+++ src/test/java/com/spring/app/attendance/AttendanceDAOTest.java
@@ -0,0 +1,14 @@
+package com.spring.app.attendance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AttendanceDAOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
