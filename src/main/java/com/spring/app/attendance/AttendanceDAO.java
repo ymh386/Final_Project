84a03b523a4741337dea59bdd1e0fe0be8fa24e8
rlajinwon <wlsdnjs652@naver.com>
2025-05-30 12:12:45 +0900

    근태관리 수정

--- src/main/java/com/spring/app/attendance/AttendanceDAO.java
+++ src/main/java/com/spring/app/attendance/AttendanceDAO.java
@@ -1,5 +1,14 @@
 package com.spring.app.attendance;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface AttendanceDAO {
 
+	
+	public int insertCheckIn(AttendanceVO Vo);
+	public int updateCheckOut(AttendanceVO Vo);
+	
+	
+	
 }
--- src/main/java/com/spring/app/attendance/AttendanceService.java
+++ src/main/java/com/spring/app/attendance/AttendanceService.java
@@ -1,5 +1,41 @@
 package com.spring.app.attendance;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AttendanceService {
+
+	@Autowired
+	public AttendanceDAO attendanceDAO;
+	
+	
+	
+	  public AttendanceVO checkIn(String username) {
+		    AttendanceVO vo = new AttendanceVO();
+		    vo.setAttendanceDate(LocalDate.now());
+		    vo.setCheckinTime(LocalTime.now());
+		    vo.setUsername(username);
+		    attendanceDAO.insertCheckIn(vo);
+		    return vo;
+	
+}
+	  
+	  	  
+	  public AttendanceVO checkOut(Long attendanceId) {
+	    AttendanceVO vo = new AttendanceVO();
+	    vo.setAttendanceId(attendanceId);
+	    vo.setCheckoutTime(LocalTime.now());
+	    attendanceDAO.updateCheckOut(vo);
+	    return vo;
+	  }
+	  
+	  
+	  
+	  
+	  
 
 }
